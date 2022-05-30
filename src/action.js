6f58960e02db871cc7a1b3905d007c547dd39dec
Janit Sri <janits_27@hotmail.com>
2022-05-29 23:28:31 -0400

    added comment feature

--- src/action.js
+++ src/action.js
@@ -1,5 +1,21 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
-    console.log("Hello World - Gitgub Action");
+    const GITHUB_TOKEN = core.getInput('githubToken');
+    const octokit = github.getOctokit(myToken);
+
+    const context = github.context;
+
+    const { pull_request } = context.payload;
+
+    await octokit.rest.issues.createComment({
+        repo: context.repo,
+        owner: context.owner,
+        issue_number: pull_request.number,
+        body: `Hello World - PR Number: ${pull_request.number}`
+    });
+
 }
 
 run();
