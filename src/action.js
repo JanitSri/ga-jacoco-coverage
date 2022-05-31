5d29d3a698cbd5e8f4bb715e225977c0c06031a4
Janit Sri <janits_27@hotmail.com>
2022-05-30 22:42:24 -0400

    updated to read dile

--- src/action.js
+++ src/action.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('githubToken');
@@ -7,17 +8,25 @@ async function run() {
 
     const context = github.context;
 
-    const { comment } = context.payload;
-    const issue = context.issue;
+    // const { comment } = context.payload;
+    // const issue = context.issue;
 
-    await octokit.rest.issues.createComment({
-        repo: issue.repo,
-        owner: issue.owner,
-        issue_number: issue.number,
-        body: `Hello World - PR Number: ${issue.number}...runnigng code coverage - ${comment.body}`
-    });
+    // await octokit.rest.issues.createComment({
+    //     repo: issue.repo,
+    //     owner: issue.owner,
+    //     issue_number: issue.number,
+    //     body: `Hello World - PR Number: ${issue.number}...runnigng code coverage - ${comment.body}`
+    // });
 
-    console.log(comment);
+    // console.log(comment);
+    
+    const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
+    const FILENAME = core.getInput('filename');
+    readFile(path.join(process.env.GITHUB_WORKSPACE, filename))
+}
+
+function readFile(filename) {
+    console.log(`Reading file: ${filename}`);
 }
 
 run();
