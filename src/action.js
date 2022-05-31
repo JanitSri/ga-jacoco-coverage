5710c47d19d91810eb8b906a5fda3303c9662cd5
Janit Sri <janits_27@hotmail.com>
2022-05-30 23:06:19 -0400

    updated to read pom file

--- src/action.js
+++ src/action.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const path = require('path');
 const fs = require('fs/promises');
+const pomParser = require("pom-parser");
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('githubToken');
@@ -24,6 +25,9 @@ async function run() {
     const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
     const FILENAME = core.getInput('filename');
     await readFile(path.join(GITHUB_WORKSPACE, FILENAME))
+
+    const POM_FILE = 'pom.xml';
+    readPom(path.join(GITHUB_WORKSPACE, POM_FILE))
 }
 
 async function readFile(filename) {
@@ -35,6 +39,20 @@ async function readFile(filename) {
     } catch (err) {
         console.log(`ERROR: getting file - ${err}`);
     }
+}
+
+function readPom(pomFile) {
+    console.log(`Reading pom.xml: ${pomFile}`);
+    pomParser.parse({ filePath: pomFile }, function(err, pomResponse) {
+        if (err) {
+          console.log("ERROR: " + err);
+          process.exit(1);
+        }
+      
+        console.log("XML: " + pomResponse.pomXml);
+        console.log("===========================");
+        console.log("OBJECT: " + JSON.stringify(pomResponse.pomObject));
+    });
 }
 
 run();
